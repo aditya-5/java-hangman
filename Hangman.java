c30de92548a221544e8be19f51f4eddac54ca02e
u07944aa <aditya@student.manchester.ac.uk>
2021-02-26 04:09:58 +0530

    Added a dioctionary and linked to driver

--- Hangman.java
+++ Hangman.java
@@ -1,16 +1,29 @@
 
 class Hangman{
 	private String playerName;
+	private String wordToGuess;
 
 	public static void main(String[] args){
 		
 	} 
 
+	Hangman(String a){
+		setPlayerName(a);
+	}
+
 	public String getPlayerName(){
 		return this.playerName;
 	}
 
+	public String getWord(){
+		return this.wordToGuess;
+	}
+
 	public void setPlayerName(String playerName){
 		this.playerName = playerName;
+	}
+
+	public void setWord(String wordToGuess){
+		this.wordToGuess = wordToGuess;
 	}
 }
--- HangmanDriver.java
+++ HangmanDriver.java
@@ -3,9 +3,12 @@ import java.io.Console;
 class HangmanDriver{
 	public static void main(String[] args){
 		Console consoleIn = System.console();
-		Hangman hm = new Hangman();
 		String inputIn = consoleIn.readLine("What is your name?");
-		hm.setPlayerName(inputIn);
+		HangmanDictionary hmd = new HangmanDictionary();
+		Hangman hm = new Hangman(inputIn);
+		hm.setWord(hmd.getWord());
 		System.out.println(hm.getPlayerName());
-	} 
+		System.out.println(hm.getWord());
+	}
+
 }
