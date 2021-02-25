63b2e6c12562a6befd249ad5c5a7a60b468972a7
u07944aa <aditya@student.manchester.ac.uk>
2021-02-26 04:01:17 +0530

    Linked the driver to the child class

--- Hangman.java
+++ Hangman.java
@@ -1,14 +1,9 @@
-import java.io.Console;
 
 class Hangman{
 	private String playerName;
 
 	public static void main(String[] args){
-		Console consoleIn = System.console();
-		Hangman hm = new Hangman();
-		String inputIn = consoleIn.readLine("What is your name?");
-		hm.setPlayerName(inputIn);
-		System.out.println(hm.getPlayerName());
+		
 	} 
 
 	public String getPlayerName(){
--- HangmanDriver.java
+++ HangmanDriver.java
@@ -3,7 +3,9 @@ import java.io.Console;
 class HangmanDriver{
 	public static void main(String[] args){
 		Console consoleIn = System.console();
+		Hangman hm = new Hangman();
 		String inputIn = consoleIn.readLine("What is your name?");
-		System.out.println(inputIn);
+		hm.setPlayerName(inputIn);
+		System.out.println(hm.getPlayerName());
 	} 
 }
