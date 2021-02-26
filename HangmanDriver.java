687100acb3a74ef04aa2f37e76f02d246d588285
u07944aa <aditya@student.manchester.ac.uk>
2021-02-26 13:28:32 +0530

    Added restart and quit feature

--- HangmanDriver.java
+++ HangmanDriver.java
@@ -2,16 +2,28 @@ import java.io.Console;
 
 class HangmanDriver{
 	public static void main(String[] args){
-		Console consoleIn = System.console();
+		
+		while (true){
+			Console consoleIn = System.console();
 		String inputIn = consoleIn.readLine("What is your name?");
+		if(inputIn.equals("quit")){
+					System.exit(0);
+				}
 		HangmanDictionary hmd = new HangmanDictionary();
 		Hangman hm = new Hangman(inputIn);
 		hm.setWord(hmd.getWord());
 		System.out.println(hm.getPlayerName());
 		String inputIn2;
+
+
+
+
 		do{
 			while(true){
 				inputIn2 = consoleIn.readLine("Type char: ");
+				if(inputIn2.equals("quit")){
+					System.exit(0);
+				}
 				if(inputIn2.length()==0 || inputIn2.length()>1)
 					continue;
 				else
@@ -21,12 +33,15 @@ class HangmanDriver{
 			hm.tryChar(inputIn2.charAt(0));
 			System.out.println(hm.getCurrentState());
 			System.out.println(hm.getRemainingGuesses());
-			System.out.println(hm.getCurrentState().equals(hm.getWord()));
 		}while(hm.getRemainingGuesses()>0 && !hm.getCurrentState().equals(hm.getWord()));
-
-		System.out.println(hm.getCurrentState());
-		System.out.println(hm.getWord());
-
+		String inputIn3 = consoleIn.readLine("Play again y/n :");
+		if(inputIn3.equals("y")){
+					continue;
+				}
+			else if(inputIn3.equals("n")){
+					System.exit(0);
+				}
+	}
 	}
 
 }
